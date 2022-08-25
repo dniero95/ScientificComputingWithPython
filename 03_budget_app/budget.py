bc60c5cbd91ad0f357e8eba6a033dd299c6c23e1
dniero95 <darioniero@icloud.com>
2022-08-25 18:23:16 +0200

    create __init__, deposit methods in budget.py

--- 03_budget_app/budget.py
+++ 03_budget_app/budget.py
@@ -1,7 +1,15 @@
 class Category:
+    def __init__(self, name):
+        self.name = name
+        self.ledger = []
+
+    def deposit(self, amount, description = ''):
+        self.ledger.append(
+            {
+                "amount": amount,
+                "description": description
+            })
 
 
 
-
-
-def create_spend_chart(categories):
+# def create_spend_chart(categories):
