895d3a5b2f76e5d18d340e0a4cefffd8b8fdf039
dniero95 <darioniero@icloud.com>
2022-08-25 17:39:42 +0200

    compleate 02_time_calculator; code to improve

--- 02_time_calculator/main.py
+++ 02_time_calculator/main.py
@@ -3,7 +3,9 @@ from time_calculator import add_time
 from unittest import main
 
 
-print(add_time("11:06 PM", "2:02"))
+# print(add_time("11:06 PM", "2:02"))
+# print(add_time("8:16 PM", "6:02", "tuesday"))
+print(add_time("11:40 AM", "0:25"))
 
 
 # Run unit tests automatically
--- 02_time_calculator/time_calculator.py
+++ 02_time_calculator/time_calculator.py
@@ -1,20 +1,11 @@
-
-
-# calc_days calculate how many days pass in a duration time given as a string hh:mm
-def calc_days(duration):
-    duration = duration.split(':')
-    hh = int(duration[0])
-    mm = int(duration[1])
-    total_minutes = mm + (hh * 60)
-    numbers_of_days = total_minutes % 1440
-
-    return numbers_of_days
-
-
+import string
 def add_time(start, duration, starting_day = ''):
 
+    # In this list I store the days of the week to use them with the optional parameter
+    day_of_the_week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+
     # convert the start time into a list
-    start = start.split(' ') # I split the string
+    start = start.split(' ') # I split the start string
     meridian = start[1]
     start_time = start[0].split(':')
     start_hour = int(start_time[0])
@@ -30,37 +21,50 @@ def add_time(start, duration, starting_day = ''):
     new_time_hour = start_hour + duration_hour
     new_time_min = start_min + duration_min
 
-    # handle min
+    # handle min if the sum is higher than 60
     if new_time_min > 60:
-        new_time_hour += (new_time_min / 60)
+        new_time_hour += (new_time_min // 60)
         new_time_min %= 60
 
 
-    if new_time_hour > 12:
-        new_time_hour %= 12
+    number_of_days = 0
+    while new_time_hour > 12:
+        new_time_hour -= 12
         if meridian == 'PM':
-            new_meridian = 'AM'
-        elif meridian == 'AM':
-            new_meridian = 'PM'
+            meridian = 'AM'
+            number_of_days += 1
+        else:
+            meridian = 'PM'
 
+    if new_time_hour == 12:
+        if meridian == 'PM':
+            meridian = 'AM'
+            number_of_days += 1
+        else:
+            meridian = 'PM'
     # add zero at the beginning if one digit time
     # the // operator does floor division
-    if new_time_hour // 10 == 0:
-        new_time_hour = f'0{new_time_hour}'
 
     if new_time_min // 10 == 0:
         new_time_min = f'0{new_time_min}'
 
+    new_time = f'{new_time_hour}:{new_time_min} {meridian}'
+
+
+    # handle optional parameter
+
+    if starting_day != '':
+        starting_day = starting_day.lower()
+        starting_day_index = day_of_the_week.index(starting_day)
+        ending_day_index = (starting_day_index + number_of_days) % 7
+        ending_day = day_of_the_week[ending_day_index]
+        new_time += f', {string.capwords(ending_day)}'
     # handle number of days
 
-    if calc_days(duration) == 0:
-        new_time = f'{new_time_hour}:{new_time_min} {new_meridian}'
-    elif calc_days(duration) == 1:
-        new_time = f'{new_time_hour}:{new_time_min} {new_meridian} (next day)'
-    elif calc_days(duration) > 1:
-        new_time = f'{new_time_hour}:{new_time_min} {new_meridian} ({calc_days(duration)} days later)'
-
-
+    if number_of_days > 1:
+        new_time += f' ({number_of_days} days later)'
+    elif number_of_days > 0:
+        new_time += f' (next day)'
 
 
 
