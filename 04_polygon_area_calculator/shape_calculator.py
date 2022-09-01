23f16eec901c8e17786eece89a469e9a43c7ba5e
dniero95 <darioniero@icloud.com>
2022-09-01 12:30:55 +0200

    start rectangle class

--- 04_polygon_area_calculator/shape_calculator.py
+++ 04_polygon_area_calculator/shape_calculator.py
@@ -1,6 +1,42 @@
 class Rectangle:
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+
+    def set_width(self, width):
+        self.__width =  width
+
+    def  set_height(self, height):
+        self.__height = height
+
+    def get_area(self):
+        return self.__width * self.__height
+
+    def get_perimeter(self):
+        return 2* (self.__width + self.__height)
+
+    def get_diagonal(self):
+        return (self.__width ** 2 + self.__height ** 2) ** .5
+
+    def get_picture(self):
+        if self.__height > 50 or self.__width > 50:
+            return f'Too big for picture.'
+
+        picture = f''
+        for i in range(self.__height):
+            for j in range(self.__width):
+                picture = f'{picture}*'
+            picture = f'{picture}\n'
+
+        return picture
+
+    def get_amount_inside(self):
+        pass
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.__width}, {self.__height})'
 
 
 
-
-class Square:
+class Square(Rectangle):
+    pass
