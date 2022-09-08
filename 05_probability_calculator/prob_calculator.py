cb6960260666bd0dd70dc5b15eb63e43086119e6
dniero95 <darioniero@icloud.com>
2022-09-08 18:53:21 +0200

    fix typo in README.md; create Hat initializer; start draw method

--- 05_probability_calculator/prob_calculator.py
+++ 05_probability_calculator/prob_calculator.py
@@ -4,5 +4,23 @@ import random
 
 class Hat:
 
+    # Here I store all Hats
+    all: list = []
+    def __init__(self, **kwargs):
+        self.balls = []
+        for key, value in kwargs.items():
+            for index in range(value):
+                self.balls.append(key)
+
+        # check if at least one ball has been passed
+        assert len(self.balls) >= 1, f'Insert at least one ball'
+
+        Hat.all.append(self)
+
+    # This method draw from the hat a number of balls at random
+
+    def draw(self, number_of_balls:int):
+        return random.choices(self.balls, k=number_of_balls)
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    pass
